f56ad77285084df3a61ecfaac0d1118a022cb91b
Eph Baum <fskirschbaum@gmail.com>
2022-07-15 08:02:17 -0700

    Update Dashboard View with some Tailwind classes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,37 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <label for="twoot">Twoot something magical:
-            <input name="twoot" type="text" placeholder="What's shakin' bacon?" />
-        </label>
-        <input class="button" type="button" name="postTwoot" value="Twoot"/>
+    <div class="flex justify-center w-screen">
+        <form action="POST">
+            <div class="min-w-fit">
+                <label for="twoot" class="form-label block mb-2 text-gray-700">Twoot something magical:
+                    <textarea class="
+                        form-control
+                        block
+                        min-w-fit
+                        px-3
+                        py-1.5
+                        text-base
+                        font-normal
+                        text-gray-700
+                        bg-white bg-clip-padding
+                        border border-solid border-gray-300
+                        rounded
+                        transition
+                        ease-in-out
+                        m-0
+                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                    "
+                    id="twootTextarea"
+                    rows="4"
+                    placeholder="What's shakin' bacon?"
+                    ></textarea>
+                </label>
+                <button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                   Twoot!
+                </button>
+        </div>
+        </form>
     </div>
 
     <div class="py-12">
