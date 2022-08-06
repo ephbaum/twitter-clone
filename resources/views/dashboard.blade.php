e4780b9c07378d24c1958c933ed3ffcc6f7fd5a3
s0up9 <soupythebloop@gmail.com>
2022-08-06 14:55:28 -0700

    :clap: It says Twoot now!!!
    updated view to display twoot on the twoot button

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -33,8 +33,7 @@
                     ></textarea>
                     <input type="hidden" name="user_id" value="{{ Auth::user()->id }}" />
                 </label>
-                <input type="submit" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
-                   Twoot!
+                <input type="submit" value="Twoot!" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                 </input>
         </div>
         </form>
